5fdf39305e3e66788b59d4a69f0e5aa0a9200a33
DavGoodman <david.petur@gmail.com>
2023-02-14 16:51:03 +0100

    initial commit

---
+++ Assets/Driver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 300;
+    [SerializeField] float moveSpeed = 20f; 
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Speed Boost") moveSpeed = boostSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime; 
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; 
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
